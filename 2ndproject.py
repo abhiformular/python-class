d9178b166fb903cdd3db095a62332417939495fd
abhiformular <54816545+abhiformular@users.noreply.github.com>
2020-07-08 17:34:29 -0700

    Add files via upload

---
+++ 2ndproject.py
@@ -0,0 +1,7 @@
+x = 1
+if x <13:
+    print('you can not get permit ')
+    print('you are still small come back later')
+    print('young child')
+else:
+    print('you can get perimit')
---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print("hello world")
+print("hello world 2")
+x = 10
+print(x)
+x = "abhi"
+print(x)
