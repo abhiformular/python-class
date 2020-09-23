1fdeb52f16a10d8d3a458f2edb6c42825484a4cc
abhiformular <54816545+abhiformular@users.noreply.github.com>
2020-09-22 18:34:38 -0700

    pythontest

---
+++ q2.py
@@ -0,0 +1,7 @@
+grade = int(input("your grade is "))
+if grade < 6:
+  print("you are in elementry school")
+elif grade > 5 and grade < 9:
+  print("you are in middle school")
+else:
+  print("you are in high school")
---
+++ q5.py
@@ -0,0 +1,6 @@
+y = int(input("choose a number "))
+
+for x in range(1,11,1):
+   table = x * y
+   print(f"{y}*{x}={table}")
+
